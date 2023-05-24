53ec9ee8726a3faa4038186a8fd0e2118a2ae783
Juliane Pires <juliane.monteiro@parceiro.mrv.com.br>
2023-05-23 22:06:08 -0300

    Adiciona configuração de estilos globais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { StylesProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+import GlobalStyle from "./commons/styles/global-style";
+
 function App() {
   return (
-    <div className="App">
+   <StylesProvider injectFirst>
+    <CssBaseline />
+    <GlobalStyle />
+    <div>
+      teste
     </div>
+   </StylesProvider>
   );
 }
 
